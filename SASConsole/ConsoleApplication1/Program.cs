96ddda76c859e0c92883f4d253fb1b981250b9d0
Carl <carlwang2006@gmail.com>
2014-05-27 13:10:48 -0400

    sas console app

---
+++ SASConsole/ConsoleApplication1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+//using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int exitcode = 0;
+            SasServer activeSession = new SasServer();
+
+            //string code = ReadFileFromAssembly("");
+
+            string path = args[0];
+            string readCode = "proc setinit; run;";
+
+            // This text is added only once to the file. 
+            if (File.Exists(path))
+            {
+               readCode = File.ReadAllText(path);
+            }
+
+            //connect to sas server
+              try
+                {
+                    activeSession.Connect();
+                }
+                catch (Exception ex)
+                {
+                   Console.WriteLine(ex.Message);
+                }
+
+            //run sas code
+            if (activeSession != null && activeSession.Workspace != null)
+            {
+                activeSession.Workspace.LanguageService.Submit(readCode);
+            }
+
+            // when code is complete, check error/warning
+            bool hasErrors = false, hasWarnings = false;
+            
+            Array carriage, lineTypes, lines;
+            do
+            {
+                activeSession.Workspace.LanguageService.FlushLogLines(1000,
+                    out carriage,
+                    out lineTypes,
+                    out lines);
+                for (int i = 0; i < lines.GetLength(0); i++)
+                {
+                    SAS.LanguageServiceLineType pre =
+                        (SAS.LanguageServiceLineType)lineTypes.GetValue(i);
+                    switch (pre)
+                    {
+                        case SAS.LanguageServiceLineType.LanguageServiceLineTypeError:
+                            hasErrors = true;
+                            break;
+                        case SAS.LanguageServiceLineType.LanguageServiceLineTypeWarning:
+                            hasWarnings = true;
+                            break;
+                        default:
+                            break;
+                    }
+                  }
+                }
+            while (lines != null && lines.Length > 0);
+
+            if (hasWarnings && hasErrors)
+            { Console.WriteLine("Program complete - has ERRORS and WARNINGS"); exitcode = 4; }
+            else if (hasErrors)
+            { Console.WriteLine("Program complete - has ERRORS"); exitcode = 4; }
+            else if (hasWarnings)
+            { Console.WriteLine("Program complete - has WARNINGS"); exitcode = 0; }
+            else
+            { Console.WriteLine("Program complete - no warnings or errors!"); exitcode = 0; }
+
+            return exitcode;
+
+        }
+
+        //string ReadFileFromAssembly(string filename)
+        //{
+        //    string filecontents = String.Empty;
+        //    Assembly assem = System.Reflection.Assembly.GetCallingAssembly();
+        //    Stream stream = assem.GetManifestResourceStream(filename);
+        //    if (stream != null)
+        //    {
+        //        StreamReader tr = new StreamReader(stream);
+        //        filecontents = tr.ReadToEnd();
+        //        tr.Close();
+        //        stream.Close();
+        //    }
+
+        //    return filecontents;
+        //}
+
+    }
+}
---
+++ SASConsole/ConsoleApplication1/SasServer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+    public class SasServer
+    {
+        /// <summary>
+        /// Name of the server ("SASApp")
+        /// </summary>
+        public string Name { get {return "Test";} }
+        /// <summary>
+        /// Node name of the server
+        /// </summary>
+        public string Host { get { return "prdsasad02"; } }
+        /// <summary>
+        /// Port (number), such as 8591
+        /// </summary>
+        public string Port { get { return "8591"; }  }
+        /// <summary>
+        /// User ID that can connect to a Workspace
+        /// </summary>
+        public string UserId { get { return "cwang"; } }
+        /// <summary>
+        /// Password to connect to the Workspace 
+        /// </summary>
+        public string Password { get { return ""; }  }
+        /// <summary>
+        /// Whether to use Local (COM) connection instead of IOM Bridge
+        /// </summary>
+  
+        // Use the ObjectKeeper, which keeps track of SAS Workspaces
+        // We need this so that the OLE DB provider can find the workspace to
+        // connect to if/when the user opens a data set to view
+        internal static SASObjectManager.ObjectKeeper objectKeeper =
+            new SASObjectManager.ObjectKeeper();
+
+        /// <summary>
+        /// Property for the SAS Workspace connection.
+        /// Will connect if needed.
+        /// </summary>
+        public SAS.Workspace Workspace
+        {
+            get
+            {
+                if (_workspace == null)
+                    Connect();
+                
+                if (_workspace!=null)
+                    return _workspace;
+                else
+                    throw new Exception("Could not connect to SAS Workspace");
+            }
+        }
+
+        /// <summary>
+        /// Is a Workspace connected?
+        /// </summary>
+        public bool IsConnected
+        {
+            get
+            {
+                return _workspace != null;     
+            }
+        }
+
+        /// <summary>
+        /// Close the Workspace if connected
+        /// </summary>
+        public void Close()
+        {
+            if (IsConnected) _workspace.Close();
+            
+            // clear out the ObjectKeeper
+            objectKeeper.RemoveAllObjects();
+
+            _workspace = null;
+        }
+
+        /// <summary>
+        /// Connect to a SAS Workspace
+        /// </summary>
+        public void Connect()
+        {
+            if (_workspace != null)
+                try
+                {
+                    Close();
+                }
+                catch { }
+                finally
+                { 
+                    _workspace = null; 
+                }
+
+                // Connect using the IOM Bridge (TCP) for remote server
+                SASObjectManager.IObjectFactory2 obObjectFactory = 
+                    new SASObjectManager.ObjectFactoryMulti2();
+                SASObjectManager.ServerDef obServer = 
+                    new SASObjectManager.ServerDef();
+                obServer.MachineDNSName = Host;
+                obServer.Protocol = SASObjectManager.Protocols.ProtocolBridge;
+                obServer.Port = Convert.ToInt32(Port);
+                obServer.ClassIdentifier = "440196d4-90f0-11d0-9f41-00a024bb830c";
+
+                // handle the case where there is no UserID or PW, and try IWA
+                // Doc on integrated Windows auth in SAS: http://support.sas.com/documentation/cdl/en/bisecag/63082/HTML/default/viewer.htm#n0go7pclcqr6q2n1bxfuk46hmtdy.htm
+                if (string.IsNullOrEmpty(UserId))
+                {
+                    obServer.BridgeSecurityPackage = "Negotiate";
+                }
+
+                _workspace = (SAS.Workspace)obObjectFactory.CreateObjectByServer(
+                    Name, true, 
+                    obServer,
+                    // if trying IWA, pass null in
+                    // otherwise try supplied credentials
+                    string.IsNullOrEmpty(UserId) ? null : UserId,
+                    string.IsNullOrEmpty(Password) ? null : Password);
+
+                objectKeeper.AddObject(1, Name, _workspace);
+
+           
+        }
+
+        private SAS.Workspace _workspace = null;
+
+
+    }
+}
