97421e22dae3a44cdb4293db0ec63bdfcd5d776b
Carl <cctester@users.noreply.github.com>
2014-05-29 11:34:03 -0400

    Update Program.cs

--- SASConsole/ConsoleApplication1/Program.cs
+++ SASConsole/ConsoleApplication1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -18,14 +20,14 @@ namespace ConsoleApplication1
             //string code = ReadFileFromAssembly("");
 
             string path = args[0];
-            string readCode = "%put &sasworklocation;";
+            string readCode = "proc setinit; run;";
 
             // This text is added only once to the file. 
             if (File.Exists(path))
             {
-               readCode = File.ReadAllText(path);
+                readCode = File.ReadAllText(path);
             }
-
+       
             //connect to sas server
               try
                 {
@@ -42,6 +44,9 @@ namespace ConsoleApplication1
                 activeSession.Workspace.LanguageService.Submit(readCode);
             }
 
+
+            PopulateDatasetList(activeSession, "work");
+
             // when code is complete, check error/warning
             bool hasErrors = false, hasWarnings = false;
             StringBuilder log = new StringBuilder();
@@ -56,6 +61,7 @@ namespace ConsoleApplication1
                 {
                     SAS.LanguageServiceLineType pre =
                         (SAS.LanguageServiceLineType)lineTypes.GetValue(i);
+                    Console.WriteLine(lines.GetValue(i));
                     switch (pre)
                     {
                         case SAS.LanguageServiceLineType.LanguageServiceLineTypeError:
@@ -67,7 +73,7 @@ namespace ConsoleApplication1
                         default:
                             break;
                     }
-                  log.Append(string.Format("{0}{1}", lines.GetValue(i) as string, Environment.NewLine));
+                    log.Append(string.Format("{0}{1}", lines.GetValue(i) as string, Environment.NewLine));
                   }
                 }
             while (lines != null && lines.Length > 0);
@@ -77,14 +83,15 @@ namespace ConsoleApplication1
                 outfile.Write(log.ToString());
             }
 
-            if (hasErrors)
+            if (hasWarnings && hasErrors)
+            { Console.WriteLine("Program complete - has ERRORS and WARNINGS"); exitcode = 4; }
+            else if (hasErrors)
             { Console.WriteLine("Program complete - has ERRORS"); exitcode = 4; }
-            else if (!File.Exists(path))
-            { Console.WriteLine("SAS code not available"); exitcode = 4; }
-            // else if (hasWarnings)
-            // { Console.WriteLine("Program complete - has WARNINGS"); exitcode = 0; }
+            else if (hasWarnings)
+            { Console.WriteLine("Program complete - has WARNINGS"); exitcode = 0; }
             else
-            { Console.WriteLine("Program complete - no ERRORS!"); exitcode = 0; }
+            { Console.WriteLine("Program complete - no warnings or errors!"); exitcode = 0; }
+
 
             return exitcode;
 
@@ -106,5 +113,87 @@ namespace ConsoleApplication1
         //    return filecontents;
         //}
 
+        static void PopulateDatasetList(SasServer ss,string libname)
+        {
+           
+                ADODB.Recordset adorecordset = new ADODB.RecordsetClass();
+                ADODB.Connection adoconnect = new ADODB.ConnectionClass();
+
+                try
+                {
+                    adoconnect.Open("Provider=sas.iomprovider.1; SAS Workspace ID=" + ss.Workspace.UniqueIdentifier, "", "", 0);
+                    // use the SASHELP.VMEMBER view to get names of all of the datasets and views in the specified library
+                    string selectclause = "select memname, memtype from sashelp.vmember where libname='" + libname + "' and memtype in ('DATA', 'VIEW')";
+                    adorecordset.Open(selectclause, adoconnect, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly, (int)ADODB.CommandTypeEnum.adCmdText);
+
+                    OleDbDataAdapter da = new OleDbDataAdapter();
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, adorecordset, "data");
+
+                }
+                catch
+                {
+
+                }
+
+                finally
+                {
+                    adoconnect.Close();
+                }
+
+        }
+
+        static Boolean AddExcelRow(String strFilePath)
+        {
+
+        if (!File.Exists(strFilePath)) return false;
+        String strExcelConn = "Provider=Microsoft.Jet.OLEDB.4.0;"+ "Data Source=" + strFilePath + ";"+ "Extended Properties='Excel 8.0;HDR=Yes'";
+        OleDbConnection connExcel = new OleDbConnection(strExcelConn);
+        OleDbCommand cmdExcel = new OleDbCommand();
+
+            try
+            {
+            cmdExcel.Connection = connExcel;
+
+    //Check if the Sheet Exists
+            connExcel.Open();
+            DataTable dtExcelSchema;
+            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            connExcel.Close();
+
+            DataRow[] dr = dtExcelSchema.Select("TABLE_NAME = 'tblData'");
+
+     //if not Create the Sheet
+
+           if (dr == null || dr.Length == 0)
+            {
+            cmdExcel.CommandText = "CREATE TABLE [tblData]" + "(ID varchar(10), Name varchar(50));";
+            connExcel.Open();
+            cmdExcel.ExecuteNonQuery();
+            connExcel.Close();
+            }
+
+     connExcel.Open();
+
+     //Add New Row to Excel File
+
+            cmdExcel.CommandText = "INSERT INTO [tblData] (ID, Name)" + " values ('1', 'MAK')";
+            cmdExcel.ExecuteNonQuery();
+            connExcel.Close();
+            return true;
+        }
+
+        catch
+        {
+        return false;
+        }
+
+        finally
+        {
+        cmdExcel.Dispose();
+        connExcel.Dispose();
+        }
+
+        }
     }
 }
